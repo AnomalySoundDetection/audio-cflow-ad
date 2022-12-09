38ecc79983145f3ec35578cd1cacd8856cd51d8f
Jieruei Chang <30610197+knosmos@users.noreply.github.com>
2022-12-08 20:02:26 -0500

    Update img_gen.py

--- img_gen.py
+++ img_gen.py
@@ -2,17 +2,41 @@
 Converts waveform data into log-mel spectrogram images.
 '''
 
+print("Importing libraries...")
+
 import common, os, cv2
+from tqdm import tqdm
 
-INPUT_DIR = '../dev_data/valve/test'
-OUTPUT_DIR = "data/DCASE/valve/test"
+for path in ["toyCar", "toyConveyor", "pump", "slider", "valve", "fan"]:
+    print("Processing", path)
 
-# iterate over all files in the directory
-for file in os.listdir(INPUT_DIR):
-    print("converting", file)
-    img = common.file_to_vector_array(os.path.join(INPUT_DIR, file))
-    # normalize
-    img += 80
-    img /= 80
-    # save
-    cv2.imwrite(os.path.join(OUTPUT_DIR, file + '.png'), img * 255)
+    INPUT_DIR = '../dev_data/' + path + "/"
+    OUTPUT_DIR = "data/DCASE/" + path + "/"
+
+    for phase in ['train', 'test']:
+        try:
+            os.makedirs(OUTPUT_DIR + phase)
+            print("Created " + OUTPUT_DIR + phase)
+        except:
+            print("Path already exists, continuing...")
+        # iterate over all files in the directory
+        for file in tqdm(os.listdir(INPUT_DIR + phase)):
+            #print("converting", file)
+            '''img = common.file_to_vector_array(os.path.join(INPUT_DIR + phase, file))
+            # normalize
+            img += 80
+            img /= 80
+            #img -= img.min()
+            #img /= img.max()
+            # save
+            '''
+            #print("converting", file)
+            img = common.file_to_vector_array(os.path.join(INPUT_DIR + phase, file), n_mels = 320, frames = 1)
+            # normalize
+            #print(img.min(), img.max())
+            img -= img.min() / 1.2
+            img /= img.max() / 1.2
+            img[img > 1] = 1
+            img[img < 0] = 0
+            img *= 255
+            cv2.imwrite(os.path.join(OUTPUT_DIR + phase, file.split(".")[0] + '.png'), img)
